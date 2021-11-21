52059239ec4bfefe8a208edfac058dcd9e768e4b
macha <marie-charlotte.calisto@epita.fr>
2021-11-21 15:32:17 +0100

    [GLOBALCONFIG] - add servers and clients initialisation

--- src/config/globalConfig.cc
+++ src/config/globalConfig.cc
@@ -3,11 +3,31 @@
 #include "globalConfig.hh"
 
 int GlobalConfig::rank = 0;
-int GlobalConfig::nb_node = 0;
-int GlobalConfig::nb_client = 0;
 
-void GlobalConfig::initConfig(int rank_, int size_) {
+//TODO: add the controller rank (and so move the other ranks: servers/clients)
+
+int GlobalConfig::nb_node = 0;
+int GlobalConfig::start_node = 0;
+int GlobalConfig::end_node = 0;
+int GlobalConfig::nb_client = 0;
+int GlobalConfig::start_client = 0;
+int GlobalConfig::end_client = 0;
+
+void GlobalConfig::initConfig(int rank_, int size) {
     rank = rank_;
-    nb_node = size_ /2;
-    nb_client = size_ - nb_node;
+
+    start_node = 0;
+    end_node = size /2;
+    nb_node = end_node - start_node;
+
+    start_client = end_node;
+    end_client = size;
+    nb_client = end_client - start_client;
+}
+
+bool GlobalConfig::is_client(int id) {
+    return (start_client <= id && id < end_client);
+}
+bool GlobalConfig::is_node(int id) {
+    return (start_node <= id && id < end_node);
 }
--- src/config/globalConfig.hh
+++ src/config/globalConfig.hh
@@ -5,17 +5,25 @@ class GlobalConfig {
 public:
     /** Initializes the global configuration
      * @param rank : GlobalConfig::rank
-     * @param size : GlobalConfig::size
+     * @param size : initialise the servers, clients and later controller ids
      */
     static void initConfig(int rank, int size);
+
+    static bool is_node(int id);
+    static bool is_client(int id);
 
     /// Current node id
     static int rank;
     /// Total number of node
     /// [0; size/2]
     static int nb_node;
+    static int end_node;
+    static int start_node;
+
     /// Total number of client
     /// [size/2; size]
     static int nb_client;
+    static int end_client;
+    static int start_client;
 };
 
